84165b9edf176b9406f3e81b32c3113e76e41fdf
Sattwik <hellosattwik@gmail.com>
2022-04-14 22:42:16 +0530

    Added Components

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import Todo from "./components/Todo";
+import "./App.css";
+
+function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  function handleChange(event) {
+    setTask((prev) => event.target.value);
+  }
+
+  function handelClick() {
+    setTasks((prev) => [...prev, task]);
+    setTask((prev) => "");
+  }
+
+  function remove(task, index) {
+    const newArray = [];
+
+    for (let i = 0; i < tasks.length; i++) {
+      if (tasks[i] !== task && i !== index) {
+        newArray.push(tasks[i]);
+      }
+    }
+
+    setTasks((prev) => newArray);
+  }
+
+  return (
+    <>
+      <div className="mt-2 flex justify-center items-center gap-2 ">
+        <input
+          onChange={handleChange}
+          value={task}
+          className="border-2 border-blue-700"
+        ></input>
+        <button
+          onClick={handelClick}
+          className="p-1 rounded-md text-xs text-slate-300 bg-violet-700"
+        >
+          Add Items
+        </button>
+      </div>
+      {tasks.map((task, index) => (
+        <Todo task={task} remove={() => remove(task, index)} key={index} />
+      ))}
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Todo.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import DoneIcon from "../Assets/circle-check-solid.svg";
+import RemoveIcon from "../Assets/circle-minus-solid.svg";
+
+export default function Todo(props) {
+  const [isDone, setIsDone] = useState(false);
+
+  function handleClick() {
+    setIsDone((prev) => !prev);
+  }
+
+  return (
+    <div className="mt-3 flex justify-center items-center gap-2 ">
+      <img onClick={handleClick} className="w-7" src={DoneIcon} alt="Done" />
+      <h2 className={isDone ? "line-through" : null}>{props.task}</h2>
+      <img
+        onClick={props.remove}
+        className="w-7"
+        src={RemoveIcon}
+        alt="Remove"
+      />
+    </div>
+  );
+}
